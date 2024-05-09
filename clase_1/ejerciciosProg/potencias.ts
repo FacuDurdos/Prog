f2d3aca1e892aed8efad23daa61a8d91c2c91863
FacuDurdos <facudurdos@gmail.com>
2024-05-09 13:19:40 -0300

    potencias.ts_v2

--- clase_1/ejerciciosProg/potencias.ts
+++ clase_1/ejerciciosProg/potencias.ts
@@ -4,13 +4,13 @@ let base:number = rls.questionInt("Ingrese la base: ");
 let exponente:number = rls.questionInt("Ingrese el exponente: ");
 
 while(exponente<0) {
-    exponente = rls.questionInt("Ingrese otro exponente: ");
+    exponente = rls.questionInt("Ingrese un exponente mayor a 0: ");
 }
 console.log(calcularPotencias(base,exponente));
 
-function calcularPotencias(nro1:number,nro2:number) {
+function calcularPotencias(base:number,exponente:number) {
     let resultado:number=0;
-    resultado=nro1**nro2;
+    resultado=base**exponente;
 return resultado;    
 }
 
