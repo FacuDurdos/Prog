f253721abb8228f8025051b9c93082d414eefdba
FacuDurdos <facudurdos@gmail.com>
2024-05-10 20:36:33 -0300

    .

---
+++ clase_1/ejerciciosProg/divisores.ts
@@ -0,0 +1,22 @@
+import * as rls from 'readline-sync';
+
+let numero:number
+let divisores:number=0;
+let noDivisores:number=0;
+
+
+function esDivisor(nro1:number,divisores:number,noDivisores:number) {
+    for(let i:number=0; i<1000; i++) {
+        if (nro1%i===0) {
+            divisores++;
+        } else if(nro1%i!=0) {
+            noDivisores++;
+        }
+    }
+return console.log("Existen",divisores,"divisores");    
+}
+
+for(let i:number=0; i<5; i++) {
+    numero=rls.questionInt("Ingrese un numero: ");
+    esDivisor(numero,divisores,noDivisores);
+}
---
+++ clase_1/ejerciciosProg/impresionDeNombre.ts
@@ -0,0 +1,29 @@
+import * as rls from 'readline-sync';
+
+let nombre:string
+let apellido:string
+let nombreCompleto:string="";
+
+
+for(let i:number=0; i<3; i++) {
+    nombre=rls.question("Ingrese el nombre: ");
+    apellido=rls.question("Ingrese el apellido: ");
+    dibujarGuiones(40);
+    imprimirNombre(nombre,apellido,nombreCompleto);
+    dibujarGuiones(40);
+}
+
+
+function imprimirNombre(nombre:string,apellido:string,nombreCompleto:string) {
+    nombreCompleto= nombre+" "+apellido;
+return console.log("El nombre completo es",nombreCompleto);        
+}
+
+
+function dibujarGuiones(cantidad:number) : string { 
+	let x:number, linea:string = "";
+	for (x=1; x<=cantidad; x++) {
+linea += "-";
+}
+return linea;
+}
